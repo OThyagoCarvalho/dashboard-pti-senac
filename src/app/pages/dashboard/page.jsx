49923807cf13ae3d82dedf60afc88a5a5f9636f4
othyagocarvalho <othyagocarvalho@outlook.com>
2023-09-18 16:05:38 -0300

    implement appointment table component

--- src/app/pages/dashboard/page.jsx
+++ src/app/pages/dashboard/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import useSession from "@/app/hooks/useSession";
 import { redirect } from "next/navigation";
 import { useSessionContext } from "@/app/context/SessionProvider";
 import ClientSearchForm from "@/app/components/client_search_form/clientSearchForm";
+import AppointmentTable from "@/app/components/appointment_table/AppointmentTable";
 
 const Dashboard = () => {
   const { hasSession } = useSessionContext();
@@ -14,25 +14,7 @@ const Dashboard = () => {
   return (
     <div className="p-4">
       <ClientSearchForm />
-      <div className="bg-white rounded-md p-4">
-        <table className="w-full border-collapse border">
-          <thead className="bg-gray-200">
-            <tr>
-              <th className="border p-2">ID</th>
-              <th className="border p-2">Name</th>
-              <th className="border p-2">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border p-2">1</td>
-              <td className="border p-2">John Doe</td>
-              <td className="border p-2">john@example.com</td>
-            </tr>
-            {/* Add more rows as needed */}
-          </tbody>
-        </table>
-      </div>
+      <AppointmentTable />
     </div>
   );
 };
