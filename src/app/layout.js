a1d61600049c90560c73fc2966a2af0a4a376e1e
othyagocarvalho <othyagocarvalho@outlook.com>
2023-09-18 18:07:19 -0300

    implement context for appointments and fix broken build

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,22 @@
+'use client'
+
 import { SessionProvider } from './context/SessionProvider'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { AppointmentsProvider } from './context/AppointmentsContext.jsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'PI Dashboard',
-  description: 'Dashboard SENAC',
-}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt_BR">
+    <html lang="pt_BR" title='PI Dashboard' description='Dashboard SENAC'>
       <body className={inter.className}>
+      <AppointmentsProvider>
         <SessionProvider>
          {children}
         </SessionProvider>
+      </AppointmentsProvider>
       </body>
     </html>
   )
